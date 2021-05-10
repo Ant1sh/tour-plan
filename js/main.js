baf06d162db7b44c55b4a2b6a65297b4f240dd49
Anton Ivanov <antishgamer@mail.ru>
2021-05-10 18:39:47 -0300

    add js + extra

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const hotelSwiper = new Swiper(".hotel-slider", {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider-button--prev",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
